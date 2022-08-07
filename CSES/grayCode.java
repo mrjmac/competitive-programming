7080ec102bc2b76e4e827e9647c04f9989949b54
mrjmac <40571030+mrjmac@users.noreply.github.com>
2022-08-07 16:11:17 -0500

    really fun problem

---
+++ CSES/grayCode.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class grayCode {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(r.readLine());
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int n = Integer.parseInt(st.nextToken());
+        add(new String[]{"0", "1"}, n, pw, 1);
+        pw.close();
+
+    }
+
+    public static void add(String[] a, int n, PrintWriter pw, int curr)
+    {
+        if (curr == n)
+        {
+            for (int i = 0; i < a.length; i++)
+            {
+                pw.println(a[i]);
+            }
+        }
+        else
+        {
+            String[] next = new String[a.length * 2];
+            for (int i = 0; i < a.length; i++)
+            {
+                next[i] = a[i] + "0";
+                next[next.length - i - 1] = a[i] + "1";
+            }
+            add(next, n, pw, curr + 1);
+        }
+        
+    }
+}
