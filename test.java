51087d9e23c59832fdcf2546d890673427f6acbc
mrjmac <40571030+mrjmac@users.noreply.github.com>
2022-08-11 21:04:57 -0500

    template files

---
+++ test.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class test {
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        StringTokenizer st = new StringTokenizer(r.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        pw.println(n);
+
+        pw.close();
+    }
+}
