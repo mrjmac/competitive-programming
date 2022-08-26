611789b5108934271a8bf8db5308aee675baa195
mrjmac <40571030+mrjmac@users.noreply.github.com>
2022-08-25 20:10:20 -0500

    a problem that certainly exists

---
+++ USACO/cowSignal.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    freopen("cowsignal.in", "r", stdin);
+	freopen("cowsignal.out", "w", stdout);
+
+    int m, n, k;
+	string line;
+	string curr;
+    cin >> m >> n >> k;
+
+	for (int i = 0; i < m * k; i++)
+	{
+		if (i % k == 0)
+		{
+			getline(cin >> ws, line);
+		}
+		for (int j = 0; j < n * k; j++)
+		{
+			if (j % k == 0)
+			{
+				curr = line.substr(j / k, 1);
+			}
+			cout << curr;
+		}
+		cout << "\n";
+	}
+}
