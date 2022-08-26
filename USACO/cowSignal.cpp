11409c545a179e0e29046027e8015f7049d6be46
mrjmac <40571030+mrjmac@users.noreply.github.com>
2022-08-25 20:41:24 -0500

    whitespcae fix?

--- USACO/cowSignal.cpp
+++ USACO/cowSignal.cpp
@@ -4,29 +4,29 @@ using namespace std;
 
 int main()
 {
-	cin.tie(0)->sync_with_stdio(0);
+    cin.tie(0)->sync_with_stdio(0);
     freopen("cowsignal.in", "r", stdin);
-	freopen("cowsignal.out", "w", stdout);
+    freopen("cowsignal.out", "w", stdout);
 
     int m, n, k;
-	string line;
-	string curr;
+    string line;
+    string curr;
     cin >> m >> n >> k;
 
-	for (int i = 0; i < m * k; i++)
+    for (int i = 0; i < m * k; i++)
+    {
+	if (i % k == 0)
 	{
-		if (i % k == 0)
-		{
-			getline(cin >> ws, line);
-		}
-		for (int j = 0; j < n * k; j++)
-		{
-			if (j % k == 0)
-			{
-				curr = line.substr(j / k, 1);
-			}
-			cout << curr;
-		}
-		cout << "\n";
+		getline(cin >> ws, line);
 	}
+	for (int j = 0; j < n * k; j++)
+	{
+	    if (j % k == 0)
+	    {
+	    	curr = line.substr(j / k, 1);
+            }
+	    cout << curr;
+	}
+        cout << "\n";
+    }
 }
