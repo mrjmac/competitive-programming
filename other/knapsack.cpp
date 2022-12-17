e21ed3522e43c78995d13f3182645ace86681e8e
mrjmac <40571030+mrjmac@users.noreply.github.com>
2022-12-16 20:37:06 -0600

    knapsack fixed lol

--- other/knapsack.cpp
+++ other/knapsack.cpp
@@ -6,43 +6,48 @@ int main()
 {
     cin.tie(0)->sync_with_stdio(0);
 
-    int n, w;
+    long long n, w;
     cin >> n >> w;
 
-    vector<pair<int, int>> gems(n + 1);
+    vector<pair<long long, long long>> gems(n + 1);
 
-    for (int i = 1; i <= n; i++)
+    for (long long i = 1; i <= n; i++)
     {
-        int a, b;
+        long long a, b;
         cin >> a >> b;
 
         gems[i] = make_pair(a, b);
     }
 
-    int dp[n + 1][w + 1];
+    long long dp[n + 1][w + 1];
 
-    for (int i = 0; i <= w; i++)
+    for (long long i = 0; i <= n; i++)
     {
         dp[i][0] = 0;
+    }
+
+    for (long long i = 0; i <= w; i++)
+    {
         dp[0][i] = 0;
     }
 
 
-    for (int rock = 1; rock <= n; rock++)
+    for (long long rock = 1; rock <= n; rock++)
     {
-        for (int weight = 1; weight <= w; weight++)
+        for (long long weight = 1; weight <= w; weight++)
         {
-            int no = dp[rock - 1][weight];
-            int yes = 0;
+            long long no = dp[rock - 1][weight];
+            long long yes = 0;
 
-            if (weight - gems[rock].second >= 0)
+            if (weight - gems[rock].first >= 0)
             {
-                yes = gems[rock].first + dp[rock - 1][weight - gems[rock].second];
+                yes = gems[rock].second + dp[rock - 1][weight - gems[rock].first];
             }
 
             dp[rock][weight] = max(yes, no);
         }
     }
+    
 
     cout << dp[n][w] << "\n";
 }
