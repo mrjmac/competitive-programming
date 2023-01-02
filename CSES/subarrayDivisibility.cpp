e2838a01eb315e964cb70c9d1798b47edece5102
mrjmac <40571030+mrjmac@users.noreply.github.com>
2023-01-01 20:23:39 -0600

    bruh

--- CSES/subarrayDivisibility.cpp
+++ CSES/subarrayDivisibility.cpp
@@ -7,25 +7,20 @@ int main()
     cin.tie(0)->sync_with_stdio(0);
 
     int n;
+    long long ans = 0, prefix = 0;
     cin >> n;
 
-    vector<long long> vals(n);
-    long long sum = 0;
-    vals[0] = 1;
+    vector<long long> mod(n);
+    mod[0] = 1;
 
     for (int i = 0; i < n; i++)
     {
         int curr;
         cin >> curr;
 
-        sum += curr;
-        vals[(sum % n + n) % n]++;
-    }
-
-    long long ans = 0;
-    for (int i = 0; i < n; i++)
-    {
-        ans += vals[i] * (vals[i] - 1) / 2;
+        prefix = (prefix + (curr % n) + n) % n;
+        ans += mod[prefix];
+        mod[prefix]++;
     }
 
     cout << ans << "\n";
