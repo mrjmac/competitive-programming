eced61a9e6bce3990a71b99dcff4bf3f7dcf65c8
mrjmac <40571030+mrjmac@users.noreply.github.com>
2023-04-02 21:47:29 -0500

    much faster dp solution to p3

--- USACO/pareidolia.cpp
+++ USACO/pareidolia.cpp
@@ -10,44 +10,25 @@ int main()
     long long ans = 0;
     cin >> s;
 
-    vector<long long> prev((long long) s.length(), 0);
+    long long dp[7] = {0};
+    long curr = s.length();
 
-    for (long long i = (long long) s.length() - 6; i >= 0; i--)
+    for (char l : s)
     {
-        if (s[i] == 'b')
+        dp[0] += 1;
+        for (int d = 5; d >= 0; d--)
         {
-            string fav = "bessie";
-            long long len = 6;
-
-            long long target = 0;
-            long long count = 0;
-            long long total = 0;
-
-            for (long long j = i; j < (long long) s.length(); j++)
+            if (l == "bessie"[d])
             {
-                if (s[j] == fav[target])
-                {
-                    target++;
-
-                    if (target == len)
-                    {
-                        count++;
-                        target = 0;
-                    }
-                }
-
-                total += count;
+                dp[d + 1] += dp[d];
+                dp[d] = 0;
             }
+        }
 
-            prev[i] = total;
-            ans += prev[i];
-            
-        }
-        else
-        {
-            prev[i] = prev[i + 1];
-            ans += prev[i];
-        }
+        ans += dp[6] * curr;
+        dp[0] += dp[6];
+        dp[6] = 0;
+        curr--;
     }
 
     cout << ans << "\n";
