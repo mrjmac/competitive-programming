4c354ebba8a0d670643a138028eb2ed3037617b8
mrjmac <40571030+mrjmac@users.noreply.github.com>
2023-04-20 09:11:58 -0500

    bruh

--- UIL/District2022/Catherine.java
+++ UIL/District2022/Catherine.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.util.*;
 
-import javax.lang.model.util.ElementScanner14;
-
 public class Catherine {
     public static void main(String args[]) throws IOException
     {
