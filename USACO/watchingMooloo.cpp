398da009fe735e47917834841510fadb3df41838
mrjmac <40571030+mrjmac@users.noreply.github.com>
2023-11-20 20:16:21 -0600

    very difficult and challenging bronze

---
+++ USACO/watchingMooloo.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<long long> days(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> days[i];
+    }
+
+    sort(days.begin(), days.end());
+
+    long long prev = days[0];
+    long long ans = k + 1;
+
+    for (int i = 1; i < n; i++)
+    {
+        ans += min(days[i] - prev, (long long) k + 1);
+        prev = days[i];
+    }
+
+    cout << ans << "\n";
+}
