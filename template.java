8c1478d195b5c98749e5ce49aa20a332a32e5afb
mrjmac <40571030+mrjmac@users.noreply.github.com>
2024-02-25 23:32:35 -0600

    make vscode not mad at me

--- template.java
+++ template.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class uilTemplate {
+public class template {
     public static void main(String args[]) throws IOException
     {
         Scanner test = new Scanner(new File(""));
